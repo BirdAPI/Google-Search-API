9c464b46ea9608686953d3fd5085f19b9a9ff86c
BirdAPI <birdapi@gmail.com>
2011-08-30 20:27:33 -0700

    google calculator results now working for the most part

--- google.py
+++ google.py
@@ -6,7 +6,7 @@ import sys
 import re
 
 __author__ = "Anthony Casagrande <birdapi@gmail.com>"
-__version__ = "0.3"
+__version__ = "0.7"
 
 """
 Represents a standard google search result
@@ -30,9 +30,10 @@ class CalculatorResult:
     def __init__(self):
         self.value = None
         self.unit = None
+        self.fullstring = None
        
     def __repr__(self):
-        return "%s %s" % (self.value, self.unit)
+        return repr([self.value, self.unit, self.fullstring])
         
 """
 Defines the public static api methods
@@ -46,31 +47,26 @@ class Google:
         results = []
         for i in range(pages):
             url = Google.get_search_url(query, i)
-            try:
-                request = urllib2.Request(url)
-                request.add_header("User-Agent", "Mozilla/5.001 (windows; U; NT4.0; en-US; rv:1.0) Gecko/25250101")
-                html = urllib2.urlopen(request).read()
-            except:
-                print "Error accessing:", url
-                break
-            soup = BeautifulSoup(html)
-            lis = soup.findAll("li", attrs = { "class" : "g" })
-            j = 0
-            for li in lis:
-                res = GoogleResult()
-                res.page = i
-                res.index = j
-                a = li.find("a")
-                res.name = a.text.strip()
-                res.link = a["href"]
-                if res.link.startswith("/search?"):
-                    # this is not an external link, so skip it
-                    continue
-                sdiv = li.find("div", attrs = { "class" : "s" })
-                if sdiv:
-                    res.description = sdiv.text.strip()
-                results.append(res)
-                j = j + 1
+            html = get_html(url)
+            if html:
+                soup = BeautifulSoup(html)
+                lis = soup.findAll("li", attrs = { "class" : "g" })
+                j = 0
+                for li in lis:
+                    res = GoogleResult()
+                    res.page = i
+                    res.index = j
+                    a = li.find("a")
+                    res.name = a.text.strip()
+                    res.link = a["href"]
+                    if res.link.startswith("/search?"):
+                        # this is not an external link, so skip it
+                        continue
+                    sdiv = li.find("div", attrs = { "class" : "s" })
+                    if sdiv:
+                        res.description = sdiv.text.strip()
+                    results.append(res)
+                    j = j + 1
         return results
     
     """
@@ -78,7 +74,18 @@ class Google:
     """
     @staticmethod
     def calculate(expr):
-        return None
+        url = Google.get_search_url(expr)
+        html = get_html(url)
+        if html:
+            soup = BeautifulSoup(html)
+            topstuff = soup.find("div", id="topstuff")
+            if topstuff:
+                a = topstuff.find("a")
+                if a["href"].find("calculator") != -1:
+                    h2 = topstuff.find("h2")
+                    if h2:
+                        return parse_calc_result(h2.text)
+        return None    
         
     @staticmethod    
     def get_search_url(query, page = 0, per_page = 10):
@@ -86,8 +93,48 @@ class Google:
         query = query.strip().replace(":", "%3A").replace("+", "%2B").replace("&", "%26").replace(" ", "+")
         return "http://www.google.com/search?hl=en&q=%s&start=%i&num=%i" % (query, page * per_page, per_page)
 
+def parse_calc_result(string):
+    result = CalculatorResult()
+    result.fullstring = string
+    string = string.strip().replace(u"\xa0", " ")
+    if string.find("=") != -1:
+        string = string[string.rfind("=") + 2:]
+    tokens = string.split(" ")
+    if len(tokens) > 0:
+        result.value = ""
+        for token in tokens:
+            if is_number(token):
+                result.value = result.value + token
+            else:
+                if result.unit:
+                    result.unit = result.unit + token
+                else:
+                    result.unit = token
+        return result
+    return None
+        
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+    
+def get_html(url):
+    try:
+        request = urllib2.Request(url)
+        request.add_header("User-Agent", "Mozilla/5.001 (windows; U; NT4.0; en-US; rv:1.0) Gecko/25250101")
+        html = urllib2.urlopen(request).read()
+        return html
+    except:
+        print "Error accessing:", url
+        return None        
+        
 def main():
     print "__main__"
+    print Google.calculate("157.3kg in grams")
+    print Google.calculate("cos(25 pi) / 17.4")
+    print "\n\n"
     results = Google.search("wikipedia")
     for result in results:
         print result, "\n"
